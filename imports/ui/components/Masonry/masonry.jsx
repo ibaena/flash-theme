5a534915b0495ef10675333bc7d9c552a612d492
ivanockupy <ivan@ockupy.com>
2017-05-30 12:19:12 -0400

    padding added

--- imports/ui/components/Masonry/masonry.jsx
+++ imports/ui/components/Masonry/masonry.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Masonry extends Component {
   render() {
     return(
-      <div>
+      <div className="mason-wrapper">
         <div id="masonry">
           <img src="//placehold.it/1200x450/992233" />
           <img src="//placehold.it/1200x450/448811" />
